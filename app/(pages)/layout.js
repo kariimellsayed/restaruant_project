03c79a0a78b7cd3fa39b638a737f519617197c6e
kariimellsayed <kariimellsayed@gmail.com>
2025-01-13 01:45:02 +0200

    active links, some work in Cart Page

--- app/(pages)/layout.js
+++ app/(pages)/layout.js
@@ -3,10 +3,7 @@ import "../globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    
-      <body
-        className="flex flex-col min-h-screen"
-      >
+      <body className="flex flex-col min-h-screen">
         <Navbar />
         {children}
       </body>

cba4fd92dc20b92995e3b5db2e2ca9254bc3f324
Hossam Zahwo <your-email@example.com>
2025-01-12 19:44:11 +0200

    Initial commit of restaurant project

---
+++ app/(pages)/about/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-10 ml-44">page</div>
+  )
+}
+
+export default page
--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,9 +1,39 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react';
 
 function Navbar() {
+  // تخزين الرابط المفعل في حالة
+  const [activeLink, setActiveLink] = useState('/menu'); // تعيين الرابط الأول كافتراضي
+
+  const navLinks = [
+    { name: 'Home', href: '/' },
+    { name: 'About', href: '/about' },
+    { name: 'Contact', href: '/contact' },
+    { name: 'Menu', href: '/menu' },
+  ];
+
   return (
-    <div className='fixed w-44 h-screen bg-black text-white flex flex-col justify-center items-center'>navbar</div>
-  )
+    <nav className='fixed w-44 h-screen flex flex-col justify-center items-center'>
+      <ul className="flex flex-col justify-center items-center p-0 bg-black text-white w-36 my-5 h-full border rounded-3xl">
+        {navLinks.map((link, index) => (
+          <li key={index}>
+            <a
+              href={link.href}
+              onClick={() => setActiveLink(link.href)} // تحديث الرابط المفعل عند النقر
+              className={`
+                block py-2 px-4 text-center m-0 w-full
+                ${activeLink === link.href ? 'bg-gray-900 text-yellow-500 font-bold' : 'hover:bg-gray-700 hover:text-yellow-400'}
+                focus:outline-none focus:ring-2 focus:ring-yellow-500
+                transition-all duration-300
+              `}
+            >
+              {link.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
